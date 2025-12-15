03ddafda902925426ee964ea6a37b9edfe2de5ee
Marc Young <marcdeeyoung@gmail.com>
2025-12-15 15:38:24 -0700

    Moved to free version.

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ resource "oci_core_instance" "ubuntu_instance" {
   # Required
   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
   compartment_id      = var.compartment_id
-  shape               = "VM.Standard.E5.Flex"
+  shape               = "VM.Standard.A1.Flex"
 
   shape_config {
     ocpus         = "4"
-    memory_in_gbs = "16"
+    memory_in_gbs = "24"
   }
 
   source_details {
